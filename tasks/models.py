c35db76bc9e13865e2b14e84a44e1ebdd3d316f3
German <polyakgerman@gmail.com>
2020-09-11 21:26:33 +0300

    [Tasks] - add signals & models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+TODO = "To Do"
+IN_PROGRESS = "In Progress"
+DONE = "Done"
+SALARY_PER_HOUR = 1000
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        (0, TODO),
+        (1, IN_PROGRESS),
+        (2, DONE)
+    ]
+
+    name = models.CharField('Name of the task', max_length=256)
+    status = models.CharField('Status of the task', max_length=16, choices=STATUS_CHOICES)
+
+class TimeStart(models.Model):
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, primary_key=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+
+class Sum(models.Model):
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, primary_key=True)
+    sum = models.FloatField()
+
+
+
---
+++ tasks/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+
+from datetime import timedelta, datetime
+
+from .models import TimeStart, Task, Sum, IN_PROGRESS, DONE, SALARY_PER_HOUR
+
+@receiver(post_save, sender=Task)
+def create_timestamp_for_inprogress(sender, instance, **kwargs):
+    if instance.status == IN_PROGRESS:
+        TimeStart.objects.create(task=instance)
+    return
+
+@receiver(post_save, sender=Task)
+def create_sum_for_done(sender, instance, **kwargs):
+    if instance.status == DONE:
+        delta = datetime.now() - instance.timestart
+        Sum.objects.create(sum=SALARY_PER_HOUR*(delta.seconds // 3600))
+    return
+
+@receiver(post_save, sender=Task)
+def delete_timestamp_for_done(sender, instance, **kwargs):
+    if instance.status == DONE:
+        instance.timestamp.delete()
+    return
